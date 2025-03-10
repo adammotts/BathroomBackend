7a0805f33a23d945bc1b7e7ec43d1eb56bebd9cd
Adam Ma <adam.zym@gmail.com>
2025-03-10 15:12:46 -0400

    ...

--- app/models/user.py
+++ app/models/user.py
@@ -20,6 +20,16 @@ class User:
     async def get_all(self):
         return await self.collection.find().to_list(1000)
     
+    async def check_existing_username_and_email(self, username: str, email: EmailStr) -> bool:
+        existing_user = await self.collection.find_one({
+            "$or": [
+                {"email": email.lower()},
+                {"username": username.lower()}
+            ]
+        })
+
+        return existing_user is not None
+
     async def create_user(self, form_data):
         return await self.collection.insert_one(form_data)
 
